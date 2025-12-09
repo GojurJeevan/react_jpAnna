fac9212a3f9515741bacb0300c31fa349de908c6
GojurJeevan <jeevangojur2605@gmail.com>
2025-12-09 12:57:41 +0530

    Update

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,14 +1,65 @@
+import { useState } from 'react'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+
 export default function Sidebar() {
+  const [open, setOpen] = useState(true)
+
   return (
-    <>
-      <div>
-        <form>
-          <input type="text" placeholder="firstName" /> <br />
-          <input type="email" placeholder="Email" /> <br />
-          <input type="phone" placeholder="Mobile"/> <br />
-          <input type="text" placeholder="Gender" />
-        </form>
-      </div>
-    </>
-  );
+    <div>
+      <button
+        onClick={() => setOpen(true)}
+        className="rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20"
+      >
+        Open drawer
+      </button>
+      <Dialog open={open} onClose={setOpen} className="relative z-10">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/50 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+        />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+              <DialogPanel
+                transition
+                className="pointer-events-auto relative w-screen max-w-md transform transition duration-500 ease-in-out data-closed:translate-x-full sm:duration-700"
+              >
+                <TransitionChild>
+                  <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 duration-500 ease-in-out data-closed:opacity-0 sm:-ml-10 sm:pr-4">
+                    <button
+                      type="button"
+                      onClick={() => setOpen(false)}
+                      className="relative rounded-md text-gray-400 hover:text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                    >
+                      <span className="absolute -inset-2.5" />
+                      <span className="sr-only">Close panel</span>
+                      <XMarkIcon aria-hidden="true" className="size-6" />
+                    </button>
+                  </div>
+                </TransitionChild>
+                <div className="relative flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl after:absolute after:inset-y-0 after:left-0 after:w-px after:bg-white/10">
+                  <div className="px-4 sm:px-6">
+                    <DialogTitle className="font-semibold text-2xl">User Details</DialogTitle>
+                  </div>
+                  <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                    <form>
+                       <div>
+                            <input type="text" placeholder='FirstName'className='border my-3'/> <br />
+                            <input type="email" placeholder='Email' className='border my-3'/> <br />
+                            <input type="mobile" placeholder='Mobile' className='border my-3'/> <br />
+                            <input type="text" placeholder='Gender' className='border my-3'/>
+                       </div>
+                    </form>
+                    <button className='bg-green-500 cursor-pointer p-1 my-2'>Save Edit</button>
+                  </div>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  )
 }
